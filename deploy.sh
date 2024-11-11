8f74ac7c3d9ef4db33577618a429824c2428dc2c
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2024-11-11 19:11:49 +0000

    Added ip option to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,21 @@
-make clean && make && nxlink -a 192.168.1.159 enablechatcheatsnx.nro
+#!/bin/bash
+
+# Default IP address
+ip="192.168.1.159"
+
+# Parse command-line options
+while getopts ":i:" opt; do
+    case ${opt} in
+        # -i
+        i )
+            ip=$OPTARG
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# Execute commands
+make clean && make && nxlink -a "$ip" enablechatcheatsnx.nro
